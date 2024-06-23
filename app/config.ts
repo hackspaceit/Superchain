e9e99f896d753b866a610dbcfd1be876a7780ade
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-06-23 16:03:59 +0700

    update

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'https://superchain-nft.vercel.app' : 'https://superchain-nft.vercel.app';
+  process.env.NODE_ENV == 'development' ? 'https://riso-gallery.vercel.app' : 'https://riso-gallery.vercel.app';
 export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xdBAA3aBdD9A488D968F316ba5752b5A3A6d056CD';
